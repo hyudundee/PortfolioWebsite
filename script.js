77e29ac46315467943784b705c6914141c758c2b
haoranyu <yu.haora@northeastern.edu>
2021-01-08 23:09:13 -0500

    effect-added

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
-    if(this.scrollY > 20){
-      $('.navbar').addClass("sticky")
-    } else {
-      $('.navbar').removeClass("sticky")
-    }
-  })
+$(document).ready(function() {
 
+  $(window).scroll(function() {
+    if(this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass("show");
+    } else {
+      $('.scroll-up-btn').removeClass("show");
+    }
+  });
+
+  $('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+  });
+  
   // toggle menu/navbar script
   $('.menu-btn').click(function(){
     $('.navbar .menu').toggleClass("active")
     $('.menu-btn em').toggleClass("active")
   })
+
+  // typing animation start
+  new Typed(".typing", {
+    strings: ["Software Developer", "Web Developer", "Young Geek", "UI Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  })
+
+  new Typed(".typing-2", {
+    strings: ["Software Developer", "Web Developer", "Young Geek", "UI Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  })
+  // owl carousel script
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: false
+      }
+    }
+  });
 })
 
-// owl carousel script
-$('.carousel').owlCarousel({
-  margin: 20,
-  loop: true,
-  autoplayTimeOut: 2000,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: false
-    },
-    600: {
-      items: 2,
-      nav: false
-    },
-    1000: {
-      items: 3,
-      nav: false
-    }
-  }
-})
